43f1490a15cf87f3f866ad3e475957b23fc609e1
Ahmed <ahmed.raza1054@gmail.com>
2023-09-30 02:38:11 +0500

    timeline functionality

--- app.js
+++ app.js
@@ -4,13 +4,37 @@ var post = document.querySelector(".post-container");
 var changedName = document.getElementById("changedName");
 
 function login() {
-  if (userName.value !== "") {
+  if (userName.value.trim() !== "") {
     changedName.innerHTML = userName.value;
     signup.style.display = "none";
     post.style.display = "block";
-    console.log("if working");
   } else {
-    alert("User not be empty");
-    console.log("else work");
+    alert("Username can't be empty");
   }
 }
+
+function changeBG(event) {
+  var clickedImage = event.target; // Get the clicked image element
+  var displayInput = document.getElementById("display");
+
+  if (clickedImage && displayInput) {
+    // Get the source (src) attribute of the clicked image
+    var newBgColor = getComputedStyle(clickedImage).backgroundImage;
+
+    // Set the background color of the display input to the clicked image's background color
+    displayInput.style.backgroundImage = newBgColor;
+  }
+}
+
+// function changeBG(event) {
+//   var clickedImage = event.target; // Get the clicked image element
+//   var displayInput = document.getElementById("display");
+
+//   if (clickedImage && displayInput) {
+//     // Get the source (src) attribute of the clicked image
+//     var newBgImage = clickedImage.getAttribute("src");
+
+//     // Set the value of the display input to the URL of the clicked image
+//     displayInput.value = newBgImage;
+//   }
+// }
