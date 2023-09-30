a927f40226de68a2ae657b771bc84996fac5cb91
Ahmed <ahmed.raza1054@gmail.com>
2023-10-01 00:41:45 +0500

    functionality added
    functionality added

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-var userName = document.getElementById("userName");
-var signup = document.querySelector(".signup-container");
-var post = document.querySelector(".post-container");
-var changedName = document.getElementById("changedName");
-var displayInput = document.getElementById("display");
+var userName = document.getElementById("userName"); // username
+var signup = document.querySelector(".signup-container"); // signup page container
+var post = document.querySelector(".post-container"); // post page container
+var changedName = document.getElementById("changedName"); // inside page showing username
+var displayInput = document.getElementById("display"); // display area
+
+// <<<<<< User upload a picture >>>>>>
 
 function loadFile(event) {
   var image = document.getElementById("userImage");
   var userPicture = document.getElementById("userPicture");
   var file = event.target.files[0];
-
   if (file) {
     image.src = URL.createObjectURL(file);
     userPicture.src = URL.createObjectURL(file);
   }
 }
+
+// >>>>>> Page change , update username & placeholder <<<<<<
 
 function login() {
   if (userName.value.trim() !== "") {
@@ -26,10 +29,17 @@ function login() {
   }
 }
 
-function updateImage() {
-  var userpicture = document.getElementById("userpicture");
-  userpicture.style.backgroundImage = "src('assests/images/friends.png')";
+// <<<<<< Post button color chnage >>>>>>
+
+function changeColors(event) {
+  if (event) {
+    var changeColor = document.querySelector(".btn-post");
+    changeColor.style.backgroundColor = "#0861F2";
+    changeColor.style.color = "#fff";
+  }
 }
+
+// <<<<<< Changing post backgroundImage >>>>>>
 
 function changeBG(event) {
   var clickedImage = event.target;
@@ -37,7 +47,9 @@ function changeBG(event) {
 
   if (clickedImage && displayInput) {
     var newBgImage = clickedImage.getAttribute("src");
-
     displayInput.style.backgroundImage = 'url("' + newBgImage + '")';
+    displayInput.color = "pink";
   }
 }
+
+function posted() {}
